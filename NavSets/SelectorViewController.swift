e9048599455c3cd4eb8fa9d24870507e97882401
Max Schuman <user133102@ny220.local>
2017-12-06 19:38:25 -0500

    Make transportation mode buttons green with white background when selected

--- NavSets/SelectorViewController.swift
+++ NavSets/SelectorViewController.swift
@@ -106,6 +106,7 @@ class SelectorViewController: UIViewController, UITextFieldDelegate, MGLMapViewD
         routeModel?.transitMode = .automobileAvoidingTraffic
         carButton?.isSelected = true
         selectedTransitButton = carButton
+        carButton.backgroundColor = UIColor.white
         
         // Set the origin field text to current location by default (and hide table because it shows on text update)
         originField.text = "Current Location"
@@ -139,6 +140,10 @@ class SelectorViewController: UIViewController, UITextFieldDelegate, MGLMapViewD
         // add rounding to corners of buttons
         self.startButton.layer.cornerRadius = 7
         self.backButton.layer.cornerRadius = 7
+        self.carButton.layer.cornerRadius = 7
+        self.bikeButton.layer.cornerRadius = 7
+        self.walkButton.layer.cornerRadius = 7
+        self.uberButton.layer.cornerRadius = 7
     }
 
     override func didReceiveMemoryWarning() {
@@ -441,6 +446,7 @@ class SelectorViewController: UIViewController, UITextFieldDelegate, MGLMapViewD
             if (button == sender) {
                 selectedTransitButton = button
                 button?.isSelected = true
+                button?.backgroundColor = UIColor.white
                 routeModel?.transitMode = MBDirectionsProfileIdentifier.automobileAvoidingTraffic
                 if (button == bikeButton) {
                     routeModel?.transitMode = MBDirectionsProfileIdentifier.cycling
@@ -457,6 +463,7 @@ class SelectorViewController: UIViewController, UITextFieldDelegate, MGLMapViewD
             }
             else {
                 button?.isSelected = false
+                button?.backgroundColor = UIColor.clear
             }
         }
     }
